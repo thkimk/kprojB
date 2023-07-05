62aac52149aa25e16544cadfcc77c75d0e6e3038
DESKTOP-49P9B8Q\김태헌 <0707thkim@gmail.com>
2023-07-05 09:15:07 +0900

    init2 commit

---
+++ src/main/java/com/thkim/kprojB/KprojBApplication.java
@@ -0,0 +1,13 @@
+package com.thkim.kprojB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KprojBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KprojBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thkim/kprojB/KprojBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thkim.kprojB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KprojBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
